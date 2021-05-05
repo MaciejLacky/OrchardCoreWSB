6e91f71aef2cf2fca7053c4986fcad5fc707600c
Maciej Łącki <maciej.lacky@gmail.com>
2021-05-05 19:50:28 +0200

    Add project files.

---
+++ src/OrchardCore.Themes/WSB-NLU/Manifest.cs
@@ -0,0 +1,11 @@
+using OrchardCore.DisplayManagement.Manifest;
+using OrchardCore.Modules.Manifest;
+
+[assembly: Theme(
+    Name = "The WSB-NLU Theme",
+    Author = ManifestConstants.OrchardCoreTeam,
+    Website = ManifestConstants.OrchardCoreWebsite,
+    Version = ManifestConstants.OrchardCoreVersion,
+    Description = "A theme adapted for new modern WSB-NLU website.",
+    Tags = new[] { "Bootstrap", "Landing page", "Liquid" }
+)]
